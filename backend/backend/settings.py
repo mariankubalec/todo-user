afb484a36d238ff2217b4d4e6ac25e49f59714b3
unknown <marian.kubalec@gmail.com>
2022-12-07 14:20:47 +0100

    render.com

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,6 +137,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
