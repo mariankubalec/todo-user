9324624ab43b739e6c178b7f49babd125b36c66a
unknown <marian.kubalec@gmail.com>
2022-12-19 11:58:02 +0100

    BE Get Todo

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,5 +1,5 @@
 import jwt
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, NotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import TodoSerializer
@@ -66,6 +66,37 @@ class ListTodoView(APIView):
         todos = Todo.objects.filter(assignee=user.id)
 
         serializer = TodoSerializer(todos, many=True)
+
+        return Response(serializer.data)
+
+
+class GetTodoView(APIView):
+
+    def get(self, request, pk):
+        # token = request.COOKIES.get('jwt')  # TODO add to utils
+        #
+        # if not token:
+        #     raise AuthenticationFailed('Unauthenticated!')
+        token = request.headers.get('Authorization')
+
+        if not token or token == '':
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = User.objects.filter(id=payload['id']).first()
+        if not user:
+            raise AuthenticationFailed('Unauthenticated - No User found!')
+
+        todo = Todo.objects.filter(id=user.id)
+
+        if not todo:
+            raise NotFound('No Todo found!')
+
+        serializer = TodoSerializer(todo)
 
         return Response(serializer.data)
 
